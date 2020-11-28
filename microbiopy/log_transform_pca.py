cf2677f93553808d053e66455f1c16417318866a
Karma Dolkar <karmadolkar29@gmail.com>
2020-11-28 16:52:29 +0530

    Log Transfrom function flake8 errors fixed

--- microbiopy/log_transform_pca.py
+++ microbiopy/log_transform_pca.py
@@ -3,21 +3,23 @@ import numpy as np
 
 
 def log_transform(count):
-  """Does logarithmic transform of matrix."""
-  total = count.sum() if count.sum()!=0 else count.max()
-  normalization = count / total
-  pc = normalization[normalization > 0].min()
-  normalization += pc
-  count = count + pc
-  lt_count = np.log(count)
+    """Does logarithmic transform of matrix."""
+    total = count.sum() if (count.sum() != 0) else count.max()
+    normalization = count / total
+    pc = normalization[normalization > 0].min()
+    normalization += pc
+    count = count + pc
+    lt_count = np.log(count)
 
-  return lt_count
+    return lt_count
+
 
 def do_pca(matrix):
-  """
-  Performs Principal Component Analysis on the matrix.
-  Reduces by one dimension.
-  """
-  m = PCA(n_components=matrix[0].size-1)
-  result = m.fit_transform(matrix)
-  return result
+    """
+    Performs Principal Component Analysis on the matrix.
+    Reduces by one dimension.
+    """
+    matrix = log_transform(matrix)
+    m = PCA(n_components=matrix[0].size-1)
+    result = m.fit_transform(matrix)
+    return result
