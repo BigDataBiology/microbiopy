6dfc345840fbd4eaffea7a89cd68c341b18ae169
Karma Dolkar <karmadolkar29@gmail.com>
2020-11-28 16:53:05 +0530

    Updated tests for filter features to take both prevalence and abundance

--- tests/test_filter_features.py
+++ tests/test_filter_features.py
@@ -1,14 +1,25 @@
+import numpy as np
+
 from microbiopy import filter_features as ff
 
 
 def test_filter_features():
     """Tests the filter_features function."""
-    matrix = [[19, 20, 21], [0, 17, 18], [0, 20, 15]]
-    x = ([[0, 20, 21], [0, 17, 18], [0, 20, 15]], [[0, 20, 21],
-         [0, 17, 18], [0, 20, 15]])
-    y = ([[0, 20, 0], [0, 17, 0], [0, 20, 0]], [[0, 20, 21],
-         [0, 17, 18], [0, 20, 15]])
-    assert ff.filter_features(
-        matrix, min_prevalence=2, min_prevalence_fraction=0.8) == x
-    assert ff.filter_features(
-        matrix, min_abundance=57, min_abundance_fraction=0.4) == y
+    matrix = [[19, 20, 21], [0, 17, 50], [0, 20, 15]]
+    x = [[0, 20, 21],
+         [0, 17, 50],
+         [0, 20, 15]]
+    y = [[0,  0, 21],
+         [0,  0, 50],
+         [0,  0, 15]]
+    z = [[0,  0, 21],
+         [0,  0, 50],
+         [0,  0, 15]]
+
+    assert np.allclose(ff.filter_features(
+        matrix, min_prevalence=2, min_prevalence_fraction=0.5), x)
+    assert np.allclose(ff.filter_features(
+        matrix, min_average_abundance=18, min_abundance_fraction=0.4), y)
+    assert np.allclose(ff.filter_features(
+        matrix, min_prevalence=1, min_prevalence_fraction=0.9,
+        min_average_abundance=18, min_abundance_fraction=0.5), z)
