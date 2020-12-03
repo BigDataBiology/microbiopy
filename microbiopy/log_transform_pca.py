ae2a1540994ed31bf9ea629d24f2932e99990570
Karma Dolkar <karmadolkar29@gmail.com>
2020-12-03 17:37:40 +0530

    Changed n_components value

--- microbiopy/log_transform_pca.py
+++ microbiopy/log_transform_pca.py
@@ -1,9 +1,11 @@
 from sklearn.decomposition import PCA
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def log_transform(count):
     """Does logarithmic transform of matrix."""
+    count = np.asarray(count)
     total = count.sum() if (count.sum() != 0) else count.max()
     normalization = count / total
     pc = normalization[normalization > 0].min()
@@ -19,7 +21,19 @@ def do_pca(matrix):
     Performs Principal Component Analysis on the matrix.
     Reduces by one dimension.
     """
+    matrix = np.asarray(matrix)
     matrix = log_transform(matrix)
-    m = PCA(n_components=matrix[0].size-1)
+    m = PCA(n_components=2)
     result = m.fit_transform(matrix)
     return result
+
+
+def generate_pca(matrix):
+    y = np.arange(len(matrix))
+    matrix = np.asarray(matrix)
+    X_reduced = do_pca(matrix)
+    plt.figure()
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, cmap='Accent')
+    plt.xlabel('First PC')
+    plt.ylabel('Second PC')
+    plt.show()
